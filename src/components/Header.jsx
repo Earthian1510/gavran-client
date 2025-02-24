0060aac015779584c7bb593dc25e30ef4b237614
Anjali Thorat <anjali.thorat1510@gmail.com>
2025-02-24 19:22:19 +0530

    feat: "Adding logout functionality"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,26 @@
 import React, { useState } from 'react'
-import { Link, NavLink, useNavigate} from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { setSearchQuery } from '../features/products/filterSlice'
 import { useDispatch } from 'react-redux'
 
+
 const Header = () => {
-    const [ search, setSearch ] = useState('');
+    const [search, setSearch] = useState('');
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-   const handleSearchSubmit = (e) => {
-    e.preventDefault()
-    dispatch(setSearchQuery(search))
-    setSearch('')
-    navigate('/products/')
-   }
+    const handleSearchSubmit = (e) => {
+        e.preventDefault()
+        dispatch(setSearchQuery(search))
+        setSearch('')
+        navigate('/products/')
+    }
 
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -25,33 +31,38 @@ const Header = () => {
                     </button>
                     <div className="collapse navbar-collapse mx-auto" id="navbarSupportedContent">
                         <form className="d-flex mx-auto" role="search">
-                            <input 
-                                className="form-control me-1" 
-                                type="search" 
-                                placeholder="Search" 
-                                aria-label="Search" 
-                                style={{ width: '300px', height: '36px'}}
+                            <input
+                                className="form-control me-1"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                                style={{ width: '300px', height: '36px' }}
                                 value={search}
                                 onChange={(e) => setSearch(e.target.value)}
                             />
-                            <button className="btn btn-sm btn-outline-success" type="submit" style={{ height: '35px', width: '35px'}} onClick={handleSearchSubmit}>
-                                <i className="bi bi-search" style={{ fontSize: '18px'}}></i>
+                            <button className="btn btn-sm btn-outline-success" type="submit" style={{ height: '35px', width: '35px' }} onClick={handleSearchSubmit}>
+                                <i className="bi bi-search" style={{ fontSize: '18px' }}></i>
                             </button>
                         </form>
                         <ul className="navbar-nav mx-2">
                             <li className="nav-item">
                                 <NavLink className="nav-link active" aria-current="page" to='/user'>
-                                    <i className="bi bi-person-circle" style={{ fontSize: '20px'}}></i>
+                                    <i className="bi bi-person-circle" style={{ fontSize: '20px' }}></i>
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link" to='/wishlist'>
-                                    <i className="bi bi-suit-heart" style={{ fontSize: '20px'}}></i>
+                                    <i className="bi bi-suit-heart" style={{ fontSize: '20px' }}></i>
                                 </NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link" to='/cart'>
-                                    <i className="bi bi-cart4" style={{ fontSize: '20px'}}></i>
+                                    <i className="bi bi-cart4" style={{ fontSize: '20px' }}></i>
+                                </NavLink>
+                            </li>
+                            <li className='nav-item'>
+                                <NavLink className='nav-link' onClick={handleLogout}>
+                                <i className="bi bi-box-arrow-right" style={{ fontSize: '20px', color: 'black' }}></i>
                                 </NavLink>
                             </li>
                         </ul>
